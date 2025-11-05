969d8858b04b1d73310e7032d775f534d5c11913
Adones <adones@hawaii.edu>
2025-11-04 20:17:07 -1000

    seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,10 +7,12 @@ const prisma = new PrismaClient();
 async function main() {
   console.log('Seeding the database');
   const password = await hash('changeme', 10);
-  config.defaultAccounts.forEach(async (account) => {
+  
+  // Handle accounts creation sequentially
+  for (const account of config.defaultAccounts) {
     const role = account.role as Role || Role.USER;
     console.log(`  Creating user: ${account.email} with role: ${role}`);
-    await prisma.user.upsert({
+    const user = await prisma.user.upsert({
       where: { email: account.email },
       update: {},
       create: {
@@ -19,8 +21,9 @@ async function main() {
         role,
       },
     });
-    // console.log(`  Created user: ${user.email} with role: ${user.role}`);
-  });
+    console.log(`  Created user: ${user.email} with role: ${user.role}`);
+  }
+
   for (const data of config.defaultData) {
     const condition = data.condition as Condition || Condition.good;
     console.log(`  Adding stuff: ${JSON.stringify(data)}`);
